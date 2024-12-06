3628df0da5a9ac06933bdb142e6f4b1882ca04b1
Adip Rijal <rizalaryan2@gmail.com>
2024-12-06 22:16:47 +0545

    commit 5

---
+++ 04_identifier.c
@@ -0,0 +1,19 @@
+// Identifiers      [ variable, function, array, constants, pointers ]
+
+/*
+
+    >> Rules for naming identifiers <<
+        ~   letters, digits and underscore ( _ ) are allowed.
+        ~   blank space is not allowed.
+        ~   must begin with a letter or underscore
+        ~   keywords are not allowed.
+        ~   is case-sensitive
+
+
+    Allowed:
+            int a, ab2, a3b, a_, _a, _, _1, _1a, A_2, _A, _A_ ;
+
+    Not allowed:
+            float 1, 1_, 1a, 1a_, a b, a_ 3 ;
+
+*/
---
+++ 05_data-types.c
@@ -0,0 +1,24 @@
+/*
+
+    Data types:
+        a)  Primary data type
+            ~   Integer (int)
+            ~   Floating point (float)
+            ~   Character (char)
+            ~   Void (void)
+            ~   Boolean (bool)
+
+        b)  User-defined data type
+            typedef <existing data type> <new name for it>
+
+            Example:
+                    typedef int integer;
+                    typedef float decimal;
+                    typedef char alpha;
+
+        c)  Derived data type
+            ~   structures
+            ~   unions
+            ~   enumerations
+
+*/
